0bf44b11a412b57babbc33544861736bc31c1d89
hiei23 <yuikazuga_25@hotmail.co.uk>
2015-11-02 14:29:20 -0500

    added some code in server/api/levels

---
+++ server/api/levels/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var express = require('express');
+var controller = require('./level.controller');
+
+var router = express.Router();
+
+router.get('/', controller.index);
+router.get('/level/:id', controller.getLevelInfo);
+module.exports = router;
