88f011737832b5319305de7a495fb452b0075e28
asakomatsuoka <mtok29@gmail.com>
2015-11-05 11:18:54 -0500

    levelone display

--- client/app/level-one/level-one.controller.js
+++ client/app/level-one/level-one.controller.js
@@ -1,30 +1,50 @@
-/**
- * Created by elsieyang on 2015-11-04.
- */
-'use strict';
-(function() {
-
-  function LevelOneController($scope, $http) {
-    var self = this;
-    this.awesomeThings = [];
-
-    //$http.get('/api/levels').then(function(response) {
-    //  self.awesomeThings = response.data;
-    //});
-
+levelOne.controller('LevelOneController', ['$scope', function($scope) {
+    $scope.modelnum = 1;
+    $scope.aliennum = 1;
     $scope.models = [];
+    $scope.aliens = [];
     var maxModels = 3;
     var maxAliens = 5;
-    for( var i =0 ; i < maxModels;i++){
-      $scope.models.push([]);
-      for( var j =0 ; j < maxAliens;j++){
-        $scope.models[i][j] = "Alien " + j + " in " + i;
-      }
-    }
+    for( var i = 0 ; i < maxModels; i++) {
+      $scope.models.push(i + 1);
+    };
+    for( var j = 0 ; j < maxAliens; j++){
+      $scope.aliens.push(j + 1);
+    };
+}]);
 
-  }
 
-  angular.module('levelOne')
-    .controller('LevelOneController', LevelOneController);
-
-})();
+///**
+// * Created by elsieyang on 2015-11-04.
+// */
+//
+//'use strict';
+//(function() {
+//
+//  function LevelOneController($scope, $http) {
+//    var self = this;
+//    this.awesomeThings = [];
+//
+//    //$http.get('/api/levels').then(function(response) {
+//    //  self.awesomeThings = response.data;
+//    //});
+//
+//    $scope.modelnum = 1;
+//    $scope.aliennum = 1;
+//    $scope.models = [];
+//    $scope.aliens = [];
+//    var maxModels = 3;
+//    var maxAliens = 5;
+//    for( var i = 0 ; i <= maxModels; i++) {
+//      $scope.models.push(i + 1);
+//    }
+//    for( var j = 0 ; j <= maxAliens; j++){
+//      $scope.aliens.push(j + 1);
+//    }
+//
+//  }
+//
+//  angular.module('levelOne')
+//    .controller('LevelOneController', LevelOneController);
+//
+//})();
--- client/app/level-one/level-one.js
+++ client/app/level-one/level-one.js
@@ -1,15 +1,17 @@
-/**
- * Created by elsieyang on 2015-11-04.
- */
-'use strict';
+///**
+// * Created by elsieyang on 2015-11-04.
+// */
+//'use strict';
+//
+//angular.module('levelOne')
+//  .config(function($stateProvider) {
+//    $stateProvider
+//      .state('level-one', {
+//        url: '/level-one',
+//        templateUrl: 'app/level-one/level-one.html',
+//        controller: 'LevelOneController',
+//        controllerAs: 'level-one'
+//      });
+//  });
 
-angular.module('levelOne')
-  .config(function($stateProvider) {
-    $stateProvider
-      .state('level-one', {
-        url: '/level-one',
-        templateUrl: 'app/level-one/level-one.html',
-        controller: 'LevelOneController',
-        controllerAs: 'level-one'
-      });
-  });
+var levelOne = angular.module('levelOne', []);
