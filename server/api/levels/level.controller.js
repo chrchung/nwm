2c47c8a045b25da495a3aa62d430c4e2f19669e5
hiei23 <yuikazuga_25@hotmail.co.uk>
2015-11-09 16:21:25 -0500

    modified getlevelinfo2 note: not returning res.json()

--- server/api/levels/level.controller.js
+++ server/api/levels/level.controller.js
@@ -37,7 +37,7 @@ exports.getLevelInfo = function(req, res)
                 levelInfo.push(results)
 
                 if (i == results.length - 1) {
-                  res.json(levelInfo);s
+                  res.json(levelInfo);
                 };
 
               },
@@ -51,7 +51,6 @@ exports.getLevelInfo = function(req, res)
           alert("Error: " + error.code + " " + error.message);
         }
       });
-
 
 
 
@@ -116,3 +115,62 @@ exports.getLevelInfo = function(req, res)
 };
 
 
+exports.getLevelInfo2 = function(req, res)
+{
+  var levelInfo = [];
+  var Levels = Parse.Object.extend("Levels");
+  var levelQuery = new Parse.Query(Levels);
+  levelQuery.equalTo("name", 1);
+  var alienList = [];
+
+  levelQuery.find({
+    success: function(results)
+    {
+      var level = results;
+      var modelsRelation = level[0].relation("model");
+      var modelsQuery = modelsRelation.query();
+      //console.log(modelQuery);
+
+      modelsQuery.find(
+        {
+          success:function(results)
+          {
+            for (var i =0; i < results.length; i++)
+            {
+              var model = results[i];
+              var Model = Parse.Object.extend("Model");
+              var Modelquery = new Parse.Query(Model);
+              var ModelPointer = { __type: "Pointer",
+                                         className: "Models",
+                                         objectId: model.id,
+                                         modelName: model.get("name")
+
+                                  };
+               Modelquery.equalTo("model", ModelPointer);
+               Modelquery.find(
+                 {
+                   success: function(results)
+                   {
+                     var attributes = [];
+                     for (var i = 0; i < results.length; i++)
+                     {
+                       var alien = results[i];
+                       attributes[i] = {"Alien" : alien.get("Alien"),
+                                        "attributes" : alien.get("attributes")};
+                     }
+                     alienList.push(attributes);
+                     console.log(attributes);
+                   },
+                 error: function(error)
+                 {alert("Error: " + error.code + " " + error.message)}})
+            }
+          },
+        error: function(error){alert("Error: " + error.code + " " + error.message)}});
+    },
+    error: function(error) {
+      alert("Error: " + error.code + " " + error.message);
+    }
+  })
+};
+
+var o = exports.getLevelInfo2();
