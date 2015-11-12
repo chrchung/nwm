957ef1ec79d278a4f98837c98caa3f17bfddcd71
elsieyoung <soupzai@163.com>
2015-11-12 13:51:14 -0500

    bug fixed from last version

--- client/app/level-one/level-one.controller.js
+++ client/app/level-one/level-one.controller.js
@@ -1,21 +1,21 @@
 angular.module('nwmApp').controller('LevelOneController', ['$scope', function($scope) {
-  $scope.models = [];      // array of model numbers
-  $scope.aliens = [];      // array of alien numbers in a model
-  $scope.num_buckets = 0;  // number of added buckets
-  $scope.alienData = {};   // mapping from an alien id to an array of (model#, alien#, bucket#)
+  $scope.alienData = [];   // mapping from an alien id to an array of (model#, alien#, bucket#)
   $scope.buckets = [];
+  $scope.num_buckets = 0;  // number of added buckets
   var maxModels = 3;       // number of models
   var maxAliens = 5;       // number of aliens in a model
 
-  for (var i = 1; i <= maxModels; i++) {
-    $scope.models.push(i);
-    for (var j = 1; j <= maxAliens; j++) {
-      $scope.alienData[i + "_" + j] = [i, j, null];
-    };
-  };
 
-  for (var j = 1; j <= maxAliens; j++) {
-    $scope.aliens.push(j);
+  // Add the first bucket
+  $scope.buckets.push({bucket:0, alien:[]});
+  $scope.num_buckets++;
+
+  for (var i = 0; i < maxModels; i++) {
+    //$scope.models.push(i);
+    $scope.alienData.push({model: i, alien: []});
+    for (var j = 0; j < maxAliens; j++) {
+      $scope.alienData[i].alien.push({alien:j, img: "app/level-one/backup_aliens/model" + i + "_" + j + ".png"});
+    };
   };
 
   $scope.selectedAlien = function (model_num, alien_num) {
@@ -24,66 +24,73 @@ angular.module('nwmApp').controller('LevelOneController', ['$scope', function($s
   };
 
   $scope.addBucket = function() {
-    var newBucket = $("#bucket" + $scope.num_buckets).clone();
-    $scope.num_buckets++;
-    newBucket.attr('id', 'bucket' + $scope.num_buckets);
-    newBucket.droppable($('.actual-bucket').droppable());
-
+    //var newBucket = $("#bucket" + $scope.num_buckets).clone();
+    //newBucket.attr('id', 'bucket' + $scope.num_buckets);
+    //alert(newBucket.data('id'));
+    //newBucket.droppable($('.actual-bucket').droppable());
     //newBucket.droppable($("#bucket" + $scope.num_buckets + " .actual-bucket").droppable('option'));
-    $(newBucket).insertBefore("#add-bucket");
+    //$(newBucket).insertBefore("#add-bucket");
+    $scope.buckets.push({bucket:$scope.num_buckets, alien:[]});
+    $scope.num_buckets++;
   };
 
-  // Add the droppable bucket id to the alianData of the alien
+
+  //Add the droppable bucket id to the alienData of the alien
   $scope.onDrop = function(event, ui) {
     var alienId = ui.draggable.attr('id');
     var bucketId = $(event.target).parent().attr('id');
-    $scope.alienData[alienId][2] = bucketId;
-    //alert($scope.alienData[alienId]);
+    var bucket = bucketId.substring(bucketId.length-1, bucketId.length);
+    var model = alienId.substring(0, 1);
+    var alien = alienId.substring(2, 3);
+    $scope.buckets[bucket].alien.push({model: model, alien: alien});
+
   };
 
   $scope.putBackAlien = function(model_num, alien_num) {
-    for (var k = 0; k <  Object.keys($scope.alienData).length; k++){
-      //alert(model_num);
-      var alien_id = Object.keys($scope.alienData)[k];
-      var bucket_id = $scope.alienData[alien_id][2];
-    }
-    if (!bucket_id) {
-      //alert(bucket_id);
-      // remove the alien
-      var deletedId = model_num + "_" + alien_num;
-      $('.model #' + deletedId).remove();
-      // add the alien back
-      $('.model #' + model_num).append("<img width='15%' src='app/level-one/backup_aliens/model" + deletedId + ".png'/>");
+    for(var m = 0; m < $scope.num_buckets; m++){
+      //alert($scope.buckets[m].alien);
+      for(var n = 0; n < ($scope.buckets[m].alien).length; n++){
+        if($scope.buckets[m].alien[n].model == model_num && $scope.buckets[m].alien[n].alien == alien_num){
+          $scope.buckets[m].alien.splice(n, 1);
+          // Now loop over alienData to find the index of the alien we want to delete.
+          for(var p = 0; p < $scope.alienData[model_num].alien.length; p++){
+            if($scope.alienData[model_num].alien[p].alien == alien_num){
+              $scope.alienData[model_num].alien.splice(p, 1);
+              $scope.alienData[model_num].alien.push({alien:alien_num,
+                img: "app/level-one/backup_aliens/model" + model_num + "_" + alien_num + ".png"})
+            }
+          }
+        }
+      }
     }
   };
 
   $scope.deleteBucket = function($event) {
-    //if($scope.num_buckets >= 1){
-      var id = $($event.target).parent().attr('id');
-      //alert(id);
-      var da_modelId, da_alienId;
-
-      $scope.num_buckets--;
-      for (var m = 0; m <  Object.keys($scope.alienData).length; m++){
-        var alien_id2 = Object.keys($scope.alienData)[m];
-        if ($scope.alienData[alien_id2][2] == id){
-          da_modelId = alien_id2.split('_')[0];
-          da_alienId = alien_id2.split('_')[1];
-          delete $scope.alienData[alien_id2][2];
-          // remove the alien
-          var deletedId2 = da_modelId + "_" + da_alienId;
-          $('.model #' + deletedId2).remove();
-          // add the alien back
-          $('.model #' + da_modelId).append("<img width='15%' src='app/level-one/backup_aliens/model" + deletedId2 + ".png'/>");
+    var id = $($event.target).parent().attr('id');
+    var bucket = id.substring(id.length-1, id.length);
+    //alert($scope.num_buckets);
+    if($scope.buckets[bucket].alien.length>0){
+      for (var m = 0; m <  $scope.buckets[bucket].alien.length; m++){
+        var model = $scope.buckets[bucket].alien[m].model;
+        var alien = $scope.buckets[bucket].alien[m].alien;
+        //alert("model: " + model + "alien: " + alien);
+        for (var n = 0; n < $scope.alienData[model].alien.length; n++){
+          if($scope.alienData[model].alien[n].alien == alien){
+            $scope.alienData[model].alien.splice(n, 1);
+            $scope.alienData[model].alien.push({alien:alien, img: "app/level-one/backup_aliens/model" + model + "_" + alien + ".png"})
+          }
         }
-
       }
-      // Remove the bucket
-      $('#' + id).remove();
-    //} else{
-    //  alert("Can't delete bucket!");
-    //}
-
+      // Set alien list in buckets[bucket] back to empty.
+      $scope.buckets[bucket].alien = [];
+    }
+    // Remove the bucket
+    for (var l = 0; l < $scope.num_buckets; l++){
+      if ($scope.buckets[l].bucket == id.substring(id.length-1, id.length)){
+        $scope.num_buckets--;
+        $scope.buckets.splice(l,1);
+      }
+    }
   };
 }]);
 //
