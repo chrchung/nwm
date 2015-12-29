81c9fe30f4689699e85e75b08eccea0d03918d86
Christina <christina@artlocalapp.com>
2015-12-29 15:10:03 -0500

    Parse key

--- server/api/levels/level.controller.js
+++ server/api/levels/level.controller.js
@@ -1,185 +1,80 @@
+/**
+ * Using Rails-like standard naming convention for endpoints.
+ * GET     /api/levels              ->  index
+ */
+
 'use strict';
+var _ = require('lodash');
+var config = require('../../config/environment');
+var Parse = require('parse').Parse;
+var fs = require('fs');
+var async = require('async');
+Parse.initialize(config.PARSE_APPID, config.PARSE_JSKEY);
 
 
-var Parse = require('parse/node').Parse;
-var APP_ID = "WEe1kGJydUpoiLzaWE0MAx4q2DAUXqDSZDOFqgHm";
-var JavaScriptKey = "CkEhBsgLQdlWHMQBijVUcSYKqsNABCMuygTqP8vt";
-Parse.initialize(APP_ID, JavaScriptKey);
-// Get list of levels
+// Returns a random game for the level specified
 exports.index = function(req, res) {
-  //res.json([]);
-};
 
-console.log("server initialized");
-
-exports.getLevelInfo = function(req, res)
-{
-  var levelInfo = [];
-  var levelQuery = Parse.Query("Levels");
-  levelQuery.equalTo(name, 1);
-
-  levelQuery.find({
-    success: function(results) {
-      var level = results;
-      var modelRelation = level.relation("model");
-      var modelQuery = modelRelation.query();
-
-      modelQuery.find({
-        success: function(results) {
-          var models = results;
-
-          var i;
-          for (i = 0; i < results.length; i++) {
-            var alienQuery = Parse.Query("Model");
-            alienQuery.equalTo(model, models[i]);
-            alienQuery.find({
-              success: function(results) {
-                levelInfo.push(results)
-
-                if (i == results.length - 1) {
-                  //res.json(levelInfo);
-                };
-
-              },
-              error: function(error) {
-                alert("Error: " + error.code + " " + error.message);
-              }
-            });
-          };
+  async.waterfall([
+    function (callback) {
+      var Games = Parse.Object.extend('Games');
+      var gamesQuery = new Parse.Query(Games);
+      gamesQuery.equalTo('level', parseInt(req.params.id));
+      gamesQuery.find({
+        success: function(games) {
+          var game = randomize(games, 1)[0];
+          callback(null, game);
         },
         error: function(error) {
-          alert("Error: " + error.code + " " + error.message);
+          res.status(500).end();
         }
       });
 
-
-
-
     },
-    error: function(error) {
-      alert("Error: " + error.code + " " + error.message);
+    function (game, callback) {
+      var models = game.relation('models');
+      var modelsQuery = models.query();
+      modelsQuery.find({
+        success: function(models) {
+          callback(null, models);
+        },
+        error: function(error) {
+          res.status(500).end();
+        }
+      });
+    },
+    function (models, callback) {
+      var arr = [];
+      async.each(models, function(model, callback) {
+        var Model = Parse.Object.extend("Model");
+        var aliensQuery = new Parse.Query(Model);
+        aliensQuery.equalTo("model", model);
+        aliensQuery.find({
+          success: function(aliens) {
+            arr.push(aliens);
+            callback();
+          },
+          error: function(error) {
+            res.status(500).end();
+          }
+        });
+      }, function(err){
+        callback(null, arr);
+      });
     }
+  ], function (err, results) {
+    if (err) {
+      res.status(500).end();
+    } else {
+      res.json(results);
+    };
   });
 };
 
 
-exports.getLevelInfo2 = function(req, res)
-{
-  var levelInfo = [];
-  var Levels = Parse.Object.extend("Levels");
-  var levelQuery = new Parse.Query(Levels);
-  levelQuery.equalTo("name", 1);
-  var alienList = [];
 
-  levelQuery.find({
-    success: function(results)
-    {
-      var level = results;
-      var modelsRelation = level[0].relation("model");
-      var modelsQuery = modelsRelation.query();
-      //console.log(modelQuery);
-
-      modelsQuery.find(
-        {
-          success:function(results)
-          {
-            for (var i =0; i < results.length; i++)
-            {
-              var model = results[i];
-              var Model = Parse.Object.extend("Model");
-              var Modelquery = new Parse.Query(Model);
-              var ModelPointer = { __type: "Pointer",
-                                         className: "Models",
-                                         objectId: model.id,
-                                         modelName: model.get("name")
-                                  };
-               Modelquery.equalTo("model", ModelPointer);
-               Modelquery.find(
-                 {
-                   success: function(results)
-                   {
-                     var attributes = [];
-                     for (var i = 0; i < results.length; i++)
-                     {
-                       var alien = results[i];
-                       attributes[i] = {"Alien" : alien.get("Alien"),
-                                        "attributes" : alien.get("attributes")};
-                     }
-                     alienList.push(attributes);
-                     console.log(alienList);
-                   },
-                 error: function(error)
-                 {alert("Error: " + error.code + " " + error.message)}})
-            }
-          },
-        error: function(error){alert("Error: " + error.code + " " + error.message)}});
-    },
-    error: function(error) {
-      alert("Error: " + error.code + " " + error.message);
-    }
-  })
+var randomize = function (arr, num) {
+  for(var j, x, i = arr.length; i; j = Math.floor(Math.random() * i), x = arr[--i], arr[i] = arr[j], arr[j] = x);
+  return arr.slice(0, num);
 };
-
-
-
-
-exports.getLevelInfo3 = function(req, res)
-{
-  var Levels = Parse.Object.extend("Levels");
-  var levelQuery = new Parse.Query(Levels);
-  levelQuery.equalTo("name", 1);
-  var alienList = [];
-
-  levelQuery.first().then(function(results)
-  {
-      var level = results;
-      var modelsRelation = level.relation("model");
-      var modelsQuery = modelsRelation.query();
-      //console.log(modelQuery);
-      return modelsQuery.find().then(function(results)
-      {
-        var promises = [];
-
-        for (var i = 0; i < results.length; i++) {
-          var model = results[i];
-          var Model = Parse.Object.extend("Model");
-          var Modelquery = new Parse.Query(Model);
-          var ModelPointer = {
-            __type: "Pointer",
-            className: "Models",
-            objectId: model.id,
-            modelName: model.get("name")
-
-          };
-          Modelquery.equalTo("model", ModelPointer);
-
-          promises.push(
-            Modelquery.find().then(function (results)
-            {
-              var attributes = [];
-                for (var i = 0; i < results.length; i++)
-                {
-                  var alien = results[i];
-                  attributes[i] = { "Alien": alien.get("Alien"),
-                                    "attributes": alien.get("attributes")
-                                  };
-                }
-             alienList.push(attributes);
-
-            }).then(function(){
-                    return alienList;},
-                    function(error)
-                    {alert("Error: " + error.code + " " + error.message)}))
-        }
-        return Parse.Promise.when(promises);
-
-      }, function(error)
-          {alert("Error: " + error.code + " " + error.message)})
-
-  }).then(function(){},
-          function(error)
-          {alert("Error: " + error.code + " " + error.message)})
-};
-var o = exports.getLevelInfo3();
-
 
