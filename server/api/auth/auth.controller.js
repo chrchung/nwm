b60ce872242f3478940becc7f777a637262eeec3
Christina <christina@artlocalapp.com>
2015-12-31 16:49:34 -0500

    Removed unnecessary functions

--- server/api/auth/auth.controller.js
+++ server/api/auth/auth.controller.js
@@ -10,46 +10,6 @@ var Parse = require('parse/node').Parse;
 Parse.initialize(config.PARSE_APPID, config.PARSE_JSKEY);
 
 
-exports.activate = function (req, res) {
-  var query = new Parse.Query(Parse.User);
-  query.get(req.params.auth_token, {
-    success: function (result) {
-      Parse.User.logIn(result.get('username'), config.TEMPWD, {
-        success: function (user) {
-          user.set('password', req.params.pwd);
-          user.save(null, {
-            error: function (userAgain, error) {
-              console.log("An error occurred.", error);
-              res.status(404).end();
-            }
-          });
-          res.status(200).end();
-        },
-        error: function (error) {
-          res.status(404).end();
-        }
-      });
-      res.status(200).end();
-    },
-    error: function (err) {
-      console.error("Failed to find user");
-      res.status(404).end();
-    }
-  });
-};
-
-exports.currentUser = function (req, res) {
-  var currentUser = Parse.User.current();
-  if (currentUser) {
-    res.json(currentUser).end();
-  } else {
-    res.status(404).end();
-  }
-};
-
-exports.userData = function (req, res) {
-  res.json(req.session.user).end();
-};
 
 exports.logout = function(req, res) {
   Parse.User.logOut();
--- server/api/auth/index.js
+++ server/api/auth/index.js
@@ -6,9 +6,7 @@ var controller = require('./auth.controller');
 var router = express.Router();
 
 router.post('/login', controller.login);
-router.post('/activate', controller.activate);
 router.get('/logout', controller.logout);
-router.get('/user_data', controller.userData);
 
 
 module.exports = router;
