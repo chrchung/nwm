9634ab61f0cfb1127a168e9e2e5e7b87c3ca5ddf
Christina <christina@artlocalapp.com>
2015-12-31 16:35:42 -0500

    frontend for user auth

---
+++ client/app/auth/auth.controller.js
@@ -0,0 +1,30 @@
+'use strict';
+
+angular.module('nwmApp')
+  .controller('AuthCtrl', function ($scope, Restangular, $state) {
+
+    $scope.username = '';
+    $scope.password = '';
+    $scope.message = '';
+
+    //Restangular.all('api/auth/').get('user_data').then(function (serverJson) {
+    //  User.user = serverJson;
+    //  if (User.user) {
+    //    Restangular.one('api/gallery/gallery_data').get().then(function(galleryData) {
+    //      galleryService.data = galleryData;
+    //    });
+    //    $state.go('main.exhibition');
+    //  }
+    //});
+
+    $scope.process = function () {
+      return Restangular.all('api/auths/login').post({
+        username: $scope.username,
+        pwd: $scope.password
+      }).then((function (data) {
+        $state.go('main');
+      }), function (err) {
+        $scope.message = "Invalid email or password";
+      });
+    };
+  });
