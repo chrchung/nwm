02c246a99421d8cd4546d4a3e9dd4251c02e5f3c
Christina <christina@artlocalapp.com>
2016-01-02 11:22:02 -0500

    Scoreboards

--- server/api/user/index.js
+++ server/api/user/index.js
@@ -7,6 +7,5 @@ var router = express.Router();
 
 router.get('/current', controller.current);
 router.post('/', controller.create);
-router.get('/all_overall', controller.getAllOverall);
 
 module.exports = router;
