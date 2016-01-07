169d2ea810ebeeccba3217d4c567c98c8041b983
elsieyoung <soupzai@163.com>
2016-01-06 22:52:00 -0500

    request data from server

--- client/app/auth/auth.controller.js
+++ client/app/auth/auth.controller.js
@@ -12,7 +12,7 @@ angular.module('nwmApp')
         username: $scope.username,
         password: $scope.password
       }).then((function (data) {
-        $state.go('scoreboard');
+        $state.go('game');
       }), function (err) {
         $scope.message = "Invalid email or password";
       });
