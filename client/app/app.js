745d12ac6b860a5fd4dddb02fbcc76c302f29d1f
asakomatsuoka <mtok29@gmail.com>
2016-01-06 22:24:01 -0500

    UI changes

--- client/app/app.js
+++ client/app/app.js
@@ -7,7 +7,8 @@ angular.module('nwmApp', [
   'ui.router',
   'ui.bootstrap',
   'ngDragDrop',
-  'restangular'
+  'restangular',
+  'pageslide-directive'
 ])
 
   .config(function($stateProvider, $urlRouterProvider, $locationProvider) {
