e8c1ffef839d23c7766c299301693af00e744eb6
Christina <christina@artlocalapp.com>
2016-01-06 19:54:18 -0500

    Changed url name

--- client/app/level-one/level-one.js
+++ client/app/level-one/level-one.js
@@ -2,8 +2,8 @@
 var levelOne = angular.module('nwmApp')
 .config(function($stateProvider) {
   $stateProvider
-    .state('levelOne', {
-      url: '/levelOne',
+    .state('game', {
+      url: '/game/:id',
       templateUrl: 'app/level-one/level-one.html',
       controller: 'LevelOneController'
     });
