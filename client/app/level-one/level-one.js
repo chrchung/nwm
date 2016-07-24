6bc07f04b0073dd5afa51cbb72d140a3bb3f3ef6
elsieyoung <soupzai@163.com>
2016-07-23 20:12:51 -0400

    fixed some bugs

--- client/app/level-one/level-one.js
+++ client/app/level-one/level-one.js
@@ -39,15 +39,42 @@ levelOne.service('database', function(Restangular, $state, aliens) {
   };
 
   /* Shuffle given array and returns the new array. */
-  this.getShuffledArray = function(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-      var j = Math.floor(Math.random() * (i + 1));
-      var temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
+  Array.prototype.shuffle = function(){
+    for (var i = 0; i < this.length; i++){
+      var a = this[i];
+      var b = Math.floor(Math.random() * this.length);
+      this[i] = this[b];
+      this[b] = a;
     }
-    return array;
-  };
+  }
+
+  this.shuffleProperties = function(obj) {
+    var new_obj = {};
+    var keys = getKeys(obj);
+    keys.shuffle();
+    for (var key in keys){
+      if (key == "shuffle") continue; // skip our prototype method
+      new_obj[keys[key]] = obj[keys[key]];
+    }
+    return new_obj;
+  }
+
+  function getKeys(obj){
+    var arr = new Array();
+    for (var key in obj)
+      arr.push(key);
+    return arr;
+  }
+  //this.getShuffledArray = function(array) {
+  //  var len = Object.keys(array).length;
+  //  for (var i = len - 1; i > 0; i--) {
+  //    var j = Math.floor(Math.random() * (i + 1));
+  //    var temp = array[i];
+  //    array[i] = array[j];
+  //    array[j] = temp;
+  //  }
+  //  return array;
+  //};
 });
 
 
