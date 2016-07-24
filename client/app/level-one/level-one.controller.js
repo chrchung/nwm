0aa544981d87a2a49ffcf89581ab96d8a6882e14
Ricecrispi <cachungchristina@gmail.com>
2016-07-24 15:45:38 -0400

    UI bug, and wasn't updating illegal aliens

--- client/app/level-one/level-one.controller.js
+++ client/app/level-one/level-one.controller.js
@@ -263,6 +263,14 @@ angular.module('nwmApp').controller('LevelOneController', function($scope, Resta
     $scope.checked = false;
     $scope.colorArray = bucket.addBucket($scope.colorArray, $scope.alienArray);
     $('#new_group').attr('disabled', true);
+
+    update.updateIllegalAlien($scope.alienArray, bucket.current_bucket);
+
+
+    // $timeout(function() {
+    //   angular.element('#color_block_' + history.historyBucketId).triggerHandler('click');
+    // }, 0);
+
   }
 
   $scope.showGroup = function(alien_id) {
