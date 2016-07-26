fd2dd21a4c7fef6cfe9a808bd5fbcff2f607a7cf
elsieyoung <soupzai@163.com>
2016-07-25 20:04:21 -0400

    fix the bug

--- client/app/level-one/level-one.js
+++ client/app/level-one/level-one.js
@@ -21,6 +21,13 @@ levelOne.directive('ngRightClick', function($parse) {
   };
 });
 
+levelOne.filter('toArray', function() { return function(obj) {
+  if (!(obj instanceof Object)) return obj;
+  return _.map(obj, function(val, key) {
+    return Object.defineProperty(val, '$key', {__proto__: null, value: key});
+  });
+}});
+
 /*******************************************************************
   DB functions / parsing functions
 *******************************************************************/
