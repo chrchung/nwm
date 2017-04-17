1770594034fbd4dbbb60f0d28e7dcb9bc16c7119
Ricecrispi <cachungchristina@gmail.com>
2017-04-16 22:03:05 -0400

    about page

---
+++ client/app/about/about.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('nwmApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('about', {
+        url: '/about',
+        templateUrl: 'app/about/about.html',
+        controller: 'AboutCtrl'
+      });
+  });
